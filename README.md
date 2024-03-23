d20456ca7ea78ea6b8e4187ee460fef72e9266df
Juan Garcia <juanmd149@gmail.com>
2024-03-22 21:02:25 -0300

    1
    upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# 📌 Aplicación de Tareas
+¡Hola! Esta es una aplicación de tareas interactiva desarrollada con React. Puedes agregar una tarea, eliminarla de la lista y marcarla como completada.
+
+
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import freeCodeCampLogo from './imagenes/lista.png';
+import ListaDeTareas from './componentes/ListaDeTareas';
+
+function App() {
+  return (
+    <div className='aplicacion-tareas'>
+      <div className='logo-contenedor'>
+        <img 
+          src={freeCodeCampLogo} 
+          className='logo' />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
